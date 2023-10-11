9c9942330fe9d387ae1420ad75c9cb983352cfc4
Chhotu735 <145634071+Chhotu735@users.noreply.github.com>
2023-10-11 12:24:39 +0530

    Day 8

---
+++ Esayhaicode.java
@@ -0,0 +1,5 @@
+class Easyhaicode{
+public static void main (String args[]){
+System.out.println("Easy hai code  Easy hai code Easy hai code  Easy hai code Easy hai code");
+}
+}
---
+++ ifelseconcept.java
@@ -0,0 +1,9 @@
+class Ifelseconcept{
+public static void main(String args[]){
+var=5;
+if(b==2)
+System.out.println("hello");
+else
+System.out.println("chandni");
+}
+}
