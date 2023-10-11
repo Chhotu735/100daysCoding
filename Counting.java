65b06b758e0bb9715d984352707dff2510fffe59
Chhotu735 <145634071+Chhotu735@users.noreply.github.com>
2023-10-11 12:22:03 +0530

    Day 7

---
+++ Counting.java
@@ -0,0 +1,11 @@
+class Counting{
+public static void main(String args[]){
+int i;
+i=1;
+while(i<=99)
+{
+System.out.println("hello");
+i=i++;
+}
+}
+}
---
+++ distinct.java
@@ -0,0 +1,14 @@
+class Distinct{
+public static void main(String args[]){
+int a=40;
+int b=45;
+int c=50;
+int d=55;
+int e=75;
+
+if(a>=75)     
+System.out.println("this Student got Distinct");
+else 
+System.out.println("this Student did not get  Distinct");
+}
+}
