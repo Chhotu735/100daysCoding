26f8982615e2d9875c894bf0df8936bc62fd9880
Chhotu735 <145634071+Chhotu735@users.noreply.github.com>
2023-10-15 13:36:02 +0530

    Add files via upload

---
+++ Parrten10.java
@@ -0,0 +1,13 @@
+class Parrten10{
+public static void main(String args[]){
+int n=1;
+for( int i=n;i<=7;i++)
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print(j+" ");
+}
+System.out.println();
+}
+ }
+   }
