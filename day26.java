eb10883fb33c02f0b0388a5a971715a531c9d333
Chhotu735 <145634071+Chhotu735@users.noreply.github.com>
2023-10-20 16:24:46 +0530

    Add files via upload

---
+++ day26.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Day26{
+public static void main(String args[]){
+Scanner sc=new Scanner (System.in);
+Scanner sc=sc.nextInt();
+int a=0;
+if(a%!=0)
+{
+System.out.prinln("num is priame");
+else 
+System.out.println("num is not priame");
+}
+}
+}
